a74c85a43041787688c0e0242f32919418d86476
eddy2195 <alexander@Alexanders-Mac.local>
2013-09-24 05:53:48 -0700

    First commit

---
+++ iCookie_Tapper/iCookie_TapperTests/iCookie_TapperTests.h
@@ -0,0 +1,13 @@
+//
+//  iCookie_TapperTests.h
+//  iCookie TapperTests
+//
+//  Created by Alexander on 9/24/13.
+//  Copyright (c) 2013 iCookie International Limited Transcorporation. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface iCookie_TapperTests : SenTestCase
+
+@end
---
+++ iCookie_Tapper/iCookie_TapperTests/iCookie_TapperTests.m
@@ -0,0 +1,32 @@
+//
+//  iCookie_TapperTests.m
+//  iCookie TapperTests
+//
+//  Created by Alexander on 9/24/13.
+//  Copyright (c) 2013 iCookie International Limited Transcorporation. All rights reserved.
+//
+
+#import "iCookie_TapperTests.h"
+
+@implementation iCookie_TapperTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in iCookie TapperTests");
+}
+
+@end
