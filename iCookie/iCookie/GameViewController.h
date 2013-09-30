49008396d11ec36c86f5fd3f327df11f230ac0dc
Olegek <olegmytsouda@mail.ru>
2013-09-30 15:56:46 +0300

    1
    1

---
+++ iCookie/iCookie/GameViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameViewController.h
+//  iCookie
+//
+//  Created by Guest on 30.09.13.
+//  Copyright (c) 2013 OC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
---
+++ iCookie/iCookie/GameViewController.m
@@ -0,0 +1,38 @@
+//
+//  GameViewController.m
+//  iCookie
+//
+//  Created by Guest on 30.09.13.
+//  Copyright (c) 2013 OC. All rights reserved.
+//
+
+#import "GameViewController.h"
+
+@interface GameViewController ()
+
+@end
+
+@implementation GameViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
